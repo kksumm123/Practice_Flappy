47308286a4fde4f6cf0e37a586a7370de4d4948c
kksumm123 <kksumm123@gmail.com>
2021-05-23 01:15:03 +0900

    스코어 스크립트 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,26 +8,30 @@ public class GameManager : MonoBehaviour
 {
     static public GameManager instance;
     public GameObject gameOverUI;
+    public TextMeshPro ScoureUI;
     bool isGameOver = false;
-
+    public int score = 0;
     void Awake()
     {
         instance = this;
         SetGamveOver(false);
+    }
+
+    void Update()
+    {
+        if (isGameOver)
+        {
+            if (Input.GetMouseButtonDown(0))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
     public void SetGamveOver(bool value)
     {
         isGameOver = value;
         gameOverUI.SetActive(value);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int value)
     {
-        if (isGameOver)
-        {
-            if (Input.GetMouseButtonDown(0))
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+        ScoureUI.text = "Score : " + score;
     }
 }
