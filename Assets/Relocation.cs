d1e6e945bf700874384b04eec725b67da4795937
kksumm123 <kksumm123@gmail.com>
2021-05-23 01:07:05 +0900

    배경 원위치 코드 수정

--- Assets/Relocation.cs
+++ Assets/Relocation.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Relocation : MonoBehaviour
 {
     [SerializeReference]
-    Vector2 ownPos;
+    float minX;
     float width;
     void Awake()
     {
         var spriterenderer = GetComponentInChildren<SpriteRenderer>();
-        ownPos = transform.position;
         width = spriterenderer.sprite.bounds.size.x * transform.lossyScale.x;
         Debug.Log($" width = {spriterenderer.sprite.bounds.size.x} * {transform.lossyScale.x}");
+        minX = -width;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(ownPos.x - transform.position.x) >= width)
+        if (transform.position.x < minX)
             transform.Translate(width * 2, 0, 0);
     }
 }
